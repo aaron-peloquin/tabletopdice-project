919e17ff090129528137fe8f1e8fed83ed603c44
Aaron <aaron4uk@gmail.com>
2018-08-15 21:50:33 -0500

    css tweak and cleanup

--- src/shared-styles.js
+++ src/shared-styles.js
@@ -20,6 +20,7 @@ $_documentContainer.innerHTML = `<dom-module id="shared-styles">
         border-radius: 5px;
         background-color: var(--app-card-background);
         box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
+        width: 100%;
       }
 
       img.logo,
@@ -32,18 +33,6 @@ $_documentContainer.innerHTML = `<dom-module id="shared-styles">
         padding: 15px;
         font-size: 16px;
         background-color: var(--app-copybox-background);
-      }
-
-      .circle {
-        display: inline-block;
-        width: 64px;
-        height: 64px;
-        text-align: center;
-        color: #555;
-        border-radius: 50%;
-        background: #ddd;
-        font-size: 30px;
-        line-height: 64px;
       }
 
       h1 {
