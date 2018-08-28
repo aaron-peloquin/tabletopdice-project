c63720473c2babc80ff12c8c488b834f6179bf85
Aaron <aaron4uk@gmail.com>
2018-08-27 22:44:17 -0500

    added 2 new SVGs

--- src/my-icons.js
+++ src/my-icons.js
@@ -1,14 +1,15 @@
 /**
  * @license
- * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ * Copyright (c) 2018 The Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at https://raw.githubusercontent.com/aaron-peloquin/tabletopdice-project/master/LICENSE
+ * The complete set of authors may be found at https://raw.githubusercontent.com/aaron-peloquin/tabletopdice-project/master/AUTHORS
  */
 
 import '@polymer/iron-iconset-svg/iron-iconset-svg.js';
+
+/**
+ * The specific icons used in my app
+ */
 const $_documentContainer = document.createElement('template');
 $_documentContainer.innerHTML = `<iron-iconset-svg name="my-icons" size="24">
   <svg>
@@ -24,6 +25,12 @@ $_documentContainer.innerHTML = `<iron-iconset-svg name="my-icons" size="24">
       </g>
       <g id="close">
         <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+      </g>
+      <g id="edit">
+        <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path>
+      </g>
+      <g id="delete">
+        <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" class="style-scope iron-icon"></path>
       </g>
     </defs>
   </svg>
