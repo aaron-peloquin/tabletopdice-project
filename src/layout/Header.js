dcd5414885d2668cedbb8981e6d7beced5a36976
Aaron <aaron4uk@gmail.com>
2018-09-30 14:01:23 -0500

    reworked elements into a more well-defined grid

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -3,25 +3,34 @@ import {
   AppBar,
   Toolbar,
   Typography,
-  Grid,
-  Button
+  Grid
 } from '@material-ui/core/'
-import Settings from './../modals/Settings'
+import {
+  Settings,
+  Sets
+} from './../modals/'
+import {
+  GoogleLogin,
+  GoogleLogout 
+} from 'react-google-login'
 
 
 function Header(PropTypes) {
   return <AppBar position="static" color="primary">
       <Toolbar>
-      <Grid container>
-          <Grid item sm>
-          <Typography variant="headline" color="textPrimary" align="center">Tabletop Dice</Typography>
-          </Grid>
-          <Grid item sm>
-            <Settings />
-          </Grid>
-          <Grid item sm>
-            <Button color="secondary" variant="contained">Settings</Button>
-          </Grid>
+      <Grid container
+        spacing={24}
+        justify="center"
+      >
+        <Grid item sm>
+        <Typography variant="headline" color="textPrimary" align="left">Tabletop Dice</Typography>
+        </Grid>
+        <Grid item xs="3" align="right">
+          <Settings />
+        </Grid>
+        <Grid item xs="3" align="left">
+          <Sets />
+        </Grid>
       </Grid>
       </Toolbar>
     </AppBar>
