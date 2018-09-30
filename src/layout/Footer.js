48a0157b056518e868b162291158cd60999c699a
Aaron <aaron4uk@gmail.com>
2018-09-30 15:33:58 -0500

    adding grid

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import {
+  Grid
+} from '@material-ui/core/'
 
 export default () =>
   <footer>
-    <p>
-      <a href="https://goo.gl/forms/HIDWZP4VsafmbQTA2">Contact</a>
-    </p>
+    <Grid container>
+      <Grid item>
+        <a href="https://goo.gl/forms/HIDWZP4VsafmbQTA2">Contact</a>
+      </Grid>
+      <Grid item></Grid>
+    </Grid>
   </footer>
