308d910cbf83f656d2c1999d66524989215efd68
Aaron <aaron4uk@gmail.com>
2018-09-29 21:54:23 -0500

    added buttons to app bar

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -2,12 +2,30 @@ import React from 'react'
 import {
   AppBar,
   Toolbar,
-  Typography
+  Typography,
+  Grid,
+  Button
 } from '@material-ui/core/'
+import Settings from './../modals/Settings'
 
-export default () =>
-  <AppBar position="static" color="primary">
-    <Toolbar>
-      <Typography variant="headline" color="textPrimary" align="center">Tabletop Dice</Typography>
-    </Toolbar>
-  </AppBar>
+
+function Header(PropTypes) {
+  return <AppBar position="static" color="primary">
+      <Toolbar>
+      <Grid container>
+          <Grid item sm>
+          <Typography variant="headline" color="textPrimary" align="center">Tabletop Dice</Typography>
+          </Grid>
+          <Grid item sm>
+            <Button color="secondary" onClick={Settings.handleOpen} variant="contained">Dice Sets</Button>
+          </Grid>
+          <Grid item sm>
+            <Button color="secondary" variant="contained">Settings</Button>
+          </Grid>
+      </Grid>
+      </Toolbar>
+      <Settings />
+    </AppBar>
+}
+
+export default Header
